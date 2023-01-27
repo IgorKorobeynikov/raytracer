7fdd8553686152d0af3c3dfca2f75bce06e80e72
linux-admin0001 <igorkorobigor@gmail.com>
2023-01-27 21:40:16 +0300

    added base material class

---
+++ material.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from glm import vec3
+
+@dataclass
+class Albedo:
+    spcular: float = 0
+    diffuse: float = 0
+
+@dataclass
+class Material:
+    color: vec3
+    specular: float
+    reflective: float
+    refractive: float
+    blurry: float
+    albedo: Albedo
+
